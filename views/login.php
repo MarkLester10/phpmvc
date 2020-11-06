9baaa959c5f8c4ec632728e93c7a07208479c5c3
Mark Lester Morta <marklester10.mlsm@gmail.com>
2020-11-06 15:47:50 +0800

    Login and Register View Refactored

--- views/login.php
+++ views/login.php
@@ -9,24 +9,31 @@
  * */
 $this->title = 'Login';
 $this->subheading = 'Get the latest news and information now';
-$this->headerBg = '../assets/img/login.jpg';
+$this->headerBg = '/assets/img/login.jpg';
 
 ?>
 
 <div class="container mt-3 mx-auto">
     <div class="row">
         <div class="col-lg-6 d-none d-lg-block">
-            <img src="../assets/img/profile.jpg" class="img-fluid w-100 h-100" alt="login image">
+            <img src="/assets/img/profile.jpg" class="img-fluid w-100 h-100" alt="login image">
         </div>
         <div class="col-lg-6 col-md-12 mx-auto">
-            <?php $form = marklester\phpmvc\form\Form::begin('', "post") ?>
-
-            <?php echo $form->field($model, 'email', 'text') ?>
-            <?php echo $form->field($model, 'password', 'password') ?>
-
-            <button type="submit" class="btn btn-primary">Login</button>
-
-            <?php marklester\phpmvc\form\Form::end() ?>
+            <form action="" method="post">
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="text" name="email" value="<?php echo $model->email ?>"
+                        class="form-control <?php echo ($model->hasError('email') ? 'is-invalid' : '') ?>">
+                    <div class="invalid-feedback"><?php echo $model->getFirstError('email') ?></div>
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" value="<?php echo $model->password ?>"
+                        class="form-control <?php echo ($model->hasError('password') ? 'is-invalid' : '') ?>">
+                    <div class="invalid-feedback"><?php echo $model->getFirstError('password') ?></div>
+                </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
         </div>
     </div>
 </div>
--- views/register.php
+++ views/register.php
@@ -10,29 +10,53 @@
 
 $this->title = 'Register';
 $this->subheading = 'Create an Account';
-$this->headerBg = '../assets/img/register.jpg';
+$this->headerBg = '/assets/img/register.jpg';
 ?>
 
 <div class="row">
     <div class="col-lg-6 d-none d-lg-block">
-        <img src="../assets/img/post-bg.jpg" class="img-fluid w-100 h-100" alt="login image">
+        <img src="/assets/img/post-bg.jpg" class="img-fluid w-100 h-100" alt="login image">
     </div>
     <div class="col-lg-6 col-md-12 mx-auto">
-        <?php $form = marklester\phpmvc\form\Form::begin('', "post") ?>
-        <div class="row">
-            <div class="col">
-                <?php echo $form->field($model, 'firstname', 'text') ?>
+        <form action="" method="post">
+            <div class="row">
+                <div class="col">
+                    <div class="form-group">
+                        <label>Firstname</label>
+                        <input type="text" name="firstname" value="<?php echo $model->firstname ?>"
+                            class="form-control <?php echo ($model->hasError('firstname') ? 'is-invalid' : '') ?>">
+                        <div class="invalid-feedback"><?php echo $model->getFirstError('firstname') ?></div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="form-group">
+                        <label>Last Name</label>
+                        <input type="text" name="lastname" value="<?php echo $model->lastname ?>"
+                            class="form-control <?php echo ($model->hasError('lastname') ? 'is-invalid' : '') ?>">
+                        <div class="invalid-feedback"><?php echo $model->getFirstError('lastname') ?></div>
+                    </div>
+                </div>
             </div>
-            <div class="col">
-                <?php echo $form->field($model, 'lastname', 'text') ?>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" name="email" value="<?php echo $model->email ?>"
+                    class="form-control <?php echo ($model->hasError('email') ? 'is-invalid' : '') ?>">
+                <div class="invalid-feedback"><?php echo $model->getFirstError('email') ?></div>
             </div>
-        </div>
-        <?php echo $form->field($model, 'email', 'text') ?>
-        <?php echo $form->field($model, 'password', 'password') ?>
-        <?php echo $form->field($model, 'confirmPassword', 'password') ?>
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" value="<?php echo $model->password ?>"
+                    class="form-control <?php echo ($model->hasError('password') ? 'is-invalid' : '') ?>">
+                <div class="invalid-feedback"><?php echo $model->getFirstError('password') ?></div>
+            </div>
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="password" name="confirmPassword" value="<?php echo $model->confirmPassword ?>"
+                    class="form-control <?php echo ($model->hasError('confirmPassword') ? 'is-invalid' : '') ?>">
+                <div class="invalid-feedback"><?php echo $model->getFirstError('confirmPassword') ?></div>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
 
-        <button type="submit" class="btn btn-primary">Submit</button>
-
-        <?php echo marklester\phpmvc\form\Form::end() ?>
+        </form>
     </div>
 </div>
